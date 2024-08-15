120e53eed2d4e9809f681c55eaf2692bb9ec73e4
dimanranawaka <dimanranawaka@gmail.com>
2024-08-16 01:51:28 +0530

    add delegate_stale.js

---
+++ delegate_stake.js
@@ -0,0 +1,16 @@
+const { Connection, clusterApiUrl } = require("@solana/web3.js");
+
+const main = async () => {
+
+
+}
+
+const runMain = async () => {
+    try {
+        await main();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+runMain();
