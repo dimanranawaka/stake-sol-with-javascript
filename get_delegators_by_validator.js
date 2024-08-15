df1b5b2a37cf87f0d82807242806ccc1a7b188a4
dimanranawaka <dimanranawaka@gmail.com>
2024-08-16 04:14:40 +0530

    implement get_delegators_by_validators.js

--- get_delegators_by_validator.js
+++ get_delegators_by_validator.js
@@ -1,8 +1,36 @@
-const { Connection, clusterApiUrl } = require("@solana/web3.js");
+const { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL, StakeProgram, Authorized, Lockup, sendAndConfirmTransaction, PublicKey } = require("@solana/web3.js");
 
 const main = async () => {
 
     const connection = new Connection(clusterApiUrl('devnet'), 'processed');
+    const STAKE_PROGRAM_ID = new PublicKey(
+        "Stake11111111111111111111111111111111111111"
+    );
+
+    const VOTE_PUB_KEY = "F2UsSsRHezY1U4h8FWMmWHkgyVd8r5hVVPNXViod9ZnJ";
+
+    const accounts = await connection.getParsedProgramAccounts(
+        STAKE_PROGRAM_ID,
+        {
+            filters: [
+                { dataSize: 200 },
+                {
+                    memcmp: {
+                        offset: 124,
+                        bytes: VOTE_PUB_KEY,
+                    },
+                },
+            ],
+        }
+    );
+
+    console.log(
+        `Total numbers of delegators found for ${VOTE_PUB_KEY} is: ${accounts.length}`
+    );
+
+    if (accounts.length) {
+        console.log(`Sample delegator : ${JSON.stringify(accounts[0])}`);
+    }
 
 }
 
