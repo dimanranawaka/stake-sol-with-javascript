b8451e2b0bcf3eae1619be949a214a79810bfe2f
dimanranawaka <dimanranawaka@gmail.com>
2024-08-16 00:35:37 +0530

    implement create_stake_account.js

--- create_stake_account.js
+++ create_stake_account.js
@@ -1,9 +1,17 @@
-const { Connection, clusterApiUrl } = require("@solana/web3.js");
+const { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } = require("@solana/web3.js");
 
 const main = async () => {
 
     const connection = new Connection(clusterApiUrl('devnet'), 'processed');
+    const wallet = Keypair.generate();
+    const airdropSignature = await connection.requestAirdrop(
+        wallet.publicKey,
+        1 * LAMPORTS_PER_SOL
+    );
 
+    await connection.confirmTransaction(airdropSignature);
+    const balance = await connection.getBalance(wallet.publicKey);
+    console.log(balance);
 }
 
 const runMain = async () => {
