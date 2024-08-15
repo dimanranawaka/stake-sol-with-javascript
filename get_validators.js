191ab2efa482f8d800e64edcb8886548b2f95c69
dimanranawaka <dimanranawaka@gmail.com>
2024-08-15 23:18:54 +0530

    implements for get validators

--- get_validators.js
+++ get_validators.js
@@ -1,5 +1,12 @@
+const { Connection, clusterApiUrl } = require("@solana/web3.js");
+
 const main = async () => {
 
+    const connection = new Connection(clusterApiUrl('devnet'), 'processed');
+    const { current, delinquent } = await connection.getVoteAccounts();
+    console.log("all validators : " + current.concat(delinquent).length);
+    console.log("current validators : " + current.length);
+    // console.log(current[0]);
 }
 
 const runMain = async () => {
